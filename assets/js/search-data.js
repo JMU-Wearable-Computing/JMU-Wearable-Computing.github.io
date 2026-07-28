1271a82abe48978af39aa0a452aa38d63fcf195b
jforsyth <forsy2jb@jmu.edu>
2026-07-28 18:57:17 +0000

    Deploying to gh-pages from @ JMU-Wearable-Computing/JMU-Wearable-Computing.github.io@c6b3e8c24bca87ed34c566f3aeb4d0579932bb98 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "Faculty, student, and alumni of the Wearable Computing Group",
+          description: "Faculty, students, and alumni of the Wearable Computing Group",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
